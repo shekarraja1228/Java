b88c56208d42e40ee4c3a6098d6ba6e54f7c60df
Raj <shekarraja1228@gmail.com>
2025-11-08 17:01:27 -0500

    Write a program to check if a HashSet is a subset of another HashSet.

---
+++ Collections/src/SubsetOfHashset.java
@@ -0,0 +1,13 @@
+import java.awt.datatransfer.StringSelection;
+import java.util.*;
+
+public class SubsetOfHashset {
+    public static void main(String[] args) {
+        List<String> list1 = Arrays.asList("Att", "Equifax", "Att", "norfolk");
+        List<String> list2 = Arrays.asList("Att", "Equifax", "Att", "norfolk", "Dexian");
+        Set<String> set1 = new HashSet<>(list1);
+        Set<String> set2 = new HashSet<>(list2);
+        boolean containsAll = set1.containsAll(set2);
+        System.out.println(containsAll);
+    }
+}
